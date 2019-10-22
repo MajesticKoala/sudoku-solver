6da5c475f13ef44aad0b0fb66816a6240726c4d6
Tom <tomas.walshy@gmail.com>
2019-10-22 17:34:12 +1100

    Cells turn red when incorrect

--- static/scripts/app.js
+++ static/scripts/app.js
@@ -1,6 +1,15 @@
 $(document).ready(function(){
     $.ajaxSetup({ cache: false });
 
+    var sudokuArray;
+    var solvedArray;
+
+    sudokuArray = [[0,0,0,0,0,0,0,0,4],[0,1,2,6,0,0,7,0,0],[9,0,8,0,0,1,0,0,6],[0,0,0,3,0,4,2,9,0],[0,0,6,0,0,9,0,0,7],[5,0,4,0,0,0,6,3,0],[0,8,0,0,0,0,0,6,0],[0,3,0,0,8,2,0,0,0],[0,0,5,7,0,0,0,0,0]];
+    solvedArray = [[7,6,3,2,9,5,8,1,4],[4,1,2,6,3,8,7,5,9],[9,5,8,4,7,1,3,2,6],[8,7,1,3,6,4,2,9,5],[3,2,6,8,5,9,1,4,7],[5,9,4,1,2,7,6,3,8],[1,8,7,9,4,3,5,6,2],[6,3,9,5,8,2,4,7,1],[2,4,5,7,1,6,9,8,3]];
+
+    populateData();
+
+
     $(".sudoku-cell").click(function(){
         //Add animation when clicking
         $(".selected").removeClass("selected");
@@ -10,7 +19,15 @@ $(document).ready(function(){
     $(".number-tray-cell").click(function(){
         //Add number to sudoku cell if empty
         var cellValue = $(this).text();
-        $(".selected").text(cellValue);
+        var selectedCell = $(".selected")
+        selectedCell.removeClass("incorrect-value")
+        selectedCell.text(cellValue);
+
+        //If selected value is wrong
+        var cellId = selectedCell.attr("id");
+        if (solvedArray[cellId[4]][cellId[5]] != cellValue) {
+            selectedCell.addClass("incorrect-value")
+        }
     });
 
     $("#generate-button").click(function(){
@@ -23,20 +40,22 @@ $(document).ready(function(){
                 dataType: "json",
                 contentType: 'application/json;charset=UTF-8',
                 error: function(request, error){
+                    $(this).removeClass("generating");
+                    $(this).text("Generate")
                     console.log(error)
                 },
                 success: function (data) {
                     $(this).removeClass("generating");
                     $(this).text("Generate")
-                    populateData(data);
+                    sudokuArray = data[0]
+                    solvedArray = data[1]
+                    populateData();
                     }
                 });
         }
     });
-    function populateData(data) {
-        sudokuArray = data[0]
-        solveArray = data[1]
-        console.log(solveArray)
+    function populateData() {
+        console.log(solvedArray)
         for (let row = 0; row < sudokuArray.length; row++) {
             for (let column = 0; column < sudokuArray[row].length; column++) {
                 //Clear out cell first
